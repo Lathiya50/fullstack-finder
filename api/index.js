2716d9d039dccf09541a5ecd86523b782a4095e7
Harmik Lathiya <harmik@qback.ai>
2024-10-19 21:03:31 +0530

    update the cors link

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://finderwebapp.netlify.app"],
+    origin: ["http://localhost:5173", "https://finder-clients.vercel.app"],
     credentials: true,
   })
 );
